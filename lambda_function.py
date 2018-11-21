173772354851daf55041a13bea07c8b1257cedaf
Snow Crash <playdatesnowcrash@gmail.com>
2018-11-21 18:31:49 +0100

    Parse and trim post text, add iso timestamp to embeds

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,11 @@
 import os
 from botocore.vendored import requests
 import json
+import html
 from datetime import datetime, timedelta
 
 this_time = datetime.utcnow().replace(second=0)
-last_time = this_time - timedelta(minutes=30)
+last_time = this_time - timedelta(minutes=5)
 webhook = os.environ['WEBHOOK']
 subreddit = os.environ['SUBREDDIT']
 headers = {
@@ -16,10 +17,11 @@ def embed(post):
     return {
         'title': post['data']['title'],
         'type': 'rich',
-        'description': post['data']['selftext'],
+        'description': html.unescape(post['data']['selftext']).replace('\n', ' ').replace('&#x200B;', '').strip()[0:280] + '...',
         'url': post['data']['url'],
         'color': 16711680,
-        'footer': {'text': 'new post from /u/' + post['data']['author']},
+        'timestamp': datetime.utcfromtimestamp(post['data']['created_utc']).isoformat(),
+        'footer': {'text': 'post from /u/' + post['data']['author']},
         'thumbnail': {'url': post['data']['url']},
         'provider': {'name': 'Reddit', 'url': 'https://www.reddit.com'}
     }
@@ -27,9 +29,12 @@ def embed(post):
 def lambda_handler(event, context):
     res = requests.get(subreddit, headers=headers).json()
     posts = res['data']['children']
+    if len(posts) < 1:
+        return
     new_posts = [p for p in posts if this_time > datetime.utcfromtimestamp(p['data']['created_utc'])  > last_time]
     
     if len(new_posts) > 0:
         embeds = [embed(p) for p in new_posts]
         data = json.dumps({'embeds': embeds})
         requests.post(webhook, headers=headers, data=data)
+        return
